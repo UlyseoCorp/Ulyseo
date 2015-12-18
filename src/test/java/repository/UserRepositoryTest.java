3d322dfe925386003015174cf556ca044f160e50
smagras <steve.magras@gmail.com>
2015-12-18 17:49:37 +0100

    add test

---
+++ src/test/java/repository/UserRepositoryTest.java
@@ -0,0 +1,15 @@
+package repository;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UserRepositoryTest {
+
+	@Test
+	public void testSalutationMessage() {
+
+		assertEquals(true, true);
+	}
+
+}
