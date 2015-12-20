2097c7da33aea4f4b28c935988451630226c8dc3
smagras <steve.magras@gmail.com>
2015-12-20 13:22:13 +0100

    #2 add some tests

--- src/test/java/unit/model/AudioElementTest.java
+++ src/test/java/unit/model/AudioElementTest.java
@@ -15,6 +15,7 @@ public class AudioElementTest extends UnitTest<AudioElement> {
 		AudioElement audioElement = new AudioElement();
 		audioElement.setDescription("description");
 		audioElement.setTitle("title");
+		audioElement.setId(1);
 
 		this.check(audioElement);
 		assertEquals(this.getErrors().isEmpty(), true);
