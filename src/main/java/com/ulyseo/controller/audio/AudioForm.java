15b93cd62f8424c5c046fd3c9309a0d18e452fec
smagras <steve.magras@gmail.com>
2016-01-14 16:01:38 +0100

    affichage des chapitres d'une collection #15

---
+++ src/main/java/com/ulyseo/controller/audio/AudioForm.java
@@ -0,0 +1,17 @@
+package com.ulyseo.controller.audio;
+
+import com.ulyseo.model.AudioCollection;
+
+public class AudioForm {
+
+	private AudioCollection audioCollection;
+
+	public AudioCollection getAudioCollection() {
+		return audioCollection;
+	}
+
+	public void setAudioCollection(AudioCollection audioCollection) {
+		this.audioCollection = audioCollection;
+	}
+
+}
